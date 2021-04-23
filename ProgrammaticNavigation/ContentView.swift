40d224a35cd2a0d4805854c07d90d29d37e09200
Chase McElroy <ichasem@gmail.com>
2021-04-23 13:02:32 -0500

    Programmatic Navigation bug of screen dismissing

--- ProgrammaticNavigation/ContentView.swift
+++ ProgrammaticNavigation/ContentView.swift
@@ -9,13 +9,45 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView(content: {
+            List {
+                NavigationLink(destination: FirstView(idUsedInSecondView: "asdf")) { Text("Programatic Navigation") }
+                NavigationLink(destination: FirstView(idUsedInSecondView: nil)) { Text("Manual Navigation") }
+            }
+        })
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct FirstView: View {
+    var idUsedInSecondView: String?
+    @State var buttonIsActive: Bool = false
+    @State var navigatedToSecondView: Bool = false
+
+    var body: some View {
+        ZStack {
+            NavigationLink(
+                destination: SecondView(commentToNavigateTo: idUsedInSecondView),
+                isActive: $buttonIsActive
+            ) {
+                Text("Hello").foregroundColor(.white)
+            }.zIndex(1)
+            Color.purple
+                .ignoresSafeArea()
+        }
+        .onAppear {
+            if idUsedInSecondView != nil && !navigatedToSecondView {
+                buttonIsActive = true
+                navigatedToSecondView = true
+            }
+        }
+    }
+}
+
+struct SecondView: View {
+    var commentToNavigateTo: String?
+
+    var body: some View {
+        Color.yellow
+            .ignoresSafeArea()
     }
 }
